c059b9531d895dfe01383435ba293dd212030b1c
iGalaxy <william@hayr.net>
2019-12-06 20:41:21 -0800

    Add module

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "Text": (text) => {
+    var TextComponent = Java.type("net.md_5.bungee.api.chat.TextComponent");
+    var HoverEvent = Java.type("net.md_5.bungee.api.chat.HoverEvent");
+    var ClickEvent = Java.type("net.md_5.bungee.api.chat.ClickEvent");
+    var ComponentBuilder = Java.type("net.md_5.bungee.api.chat.ComponentBuilder");
+
+    this.message = new TextComponent(text);
+
+    this.setHoverText = (hovText) => {
+      this.message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hovText).create()));
+      return this;
+    }
+
+    this.setCommand = (command) => {
+      this.message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command));
+      return this;
+    }
+
+    this.setUrl = (url) => {
+      this.message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+      return this;
+    }
+
+    this.addExtra = (text) => {
+      this.message.addExtra(text);
+      return this;
+    }
+
+    this.create = () => {
+      return this.message;
+    }
+
+    return this;
+  }
+}
