bb6a7b08b02d4deb12b4ea4ca2a738063f298b29
iGalaxy <william@satellyte.net>
2020-06-25 16:33:37 -0700

    TextUtils 2.0.0

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ You can download Drupi [here](https://stacket.net/drupi).
 To install **TextUtils**, start up your Minecraft server and make sure Drupi is installed.
 Once Drupi has been installed, restart your server, and type the command `/drupi install textutils` into chat.
 This will begin installing **TextUtils**.
+You can also download the module and put the files in `/plugins/Drupi/scripts/modules/TextUtils`.
 
 ## Methods
-The center of **TextUtils** is the `Text()` object, which has 5 methods.
+The center of **TextUtils** is the `Text` class, which has 12 methods.
 
 `setHoverText(string)` - add text to displayed when text is hovered over
 
@@ -20,18 +21,30 @@ The center of **TextUtils** is the `Text()` object, which has 5 methods.
 
 `addExtra(string)` - append text to the object
 
+`setColor(string)` - set the color of the text (supports hex color codes + Java colors)
+
+`setBold(boolean)` - set whether or not the text is bold
+
+`setItalic(boolean)` - set whether or not the text is italic
+
+`setUnderlined(boolean)` - set whether or not the text is underlined
+
+`setStrikethrough(boolean)` - set whether or not the text is struckthrough
+
+`setObfuscated(boolean)` - set whether or not the text is obfuscated
+
 `create()` - returns the TextComponent, ready to be used
 
 ## Broadcasting a message
 **TextUtils** is very easy to implement into your server. Let's see an example:
 
 ```js
-const Text = require('TextUtils').Text;
+const Text = require('TextUtils');
 
 event.addListener("PlayerJoinEvent", event => {
   var player = event.getPlayer();
 
-  server.spigot().broadcast(Text("A player has joined!").setHoverText(player.getDisplayName()).create());
+  server.spigot().broadcast(new Text("A player has joined!").setHoverText(player.getDisplayName()).create());
 });
 ```
 
@@ -41,19 +54,19 @@ The above script would output `A player has joined!` to every player with the ho
 If you'd only like to send a message to the player that joined, you can use the following example:
 
 ```js
-const Text = require('TextUtils').Text;
+const Text = require('TextUtils');
 
 event.addListener("PlayerJoinEvent", event => {
   var player = event.getPlayer();
 
-  player.spigot().sendMessage(Text("Welcome to the server!").setUrl("https://myserver.com").create());
+  player.spigot().sendMessage(new Text("Welcome to the server!").setUrl("https://myserver.com").create());
 });
 ```
 
 This would send a clickable message to the player that opens the server's website.
 
 ## Notes
-You can use any method with **TextUtils**, as long as it supports `TextComponent` from Bungeecord.
+You can use any method with **TextUtils**, as long as it supports `TextComponent` from the BungeeCord Chat API.
 Methods that do not support `TextComponent`, such as the normal `player.sendMessage()`, will not work with **TextUtils**.
 
 *Enjoy TextUtils? Consider checking out our [other services](https://satellyte.net).*
--- index.js
+++ index.js
@@ -1,41 +1,70 @@
-module.exports = {
-  "Text": text => {
-    var TextComponent = Java.type("net.md_5.bungee.api.chat.TextComponent");
-    var HoverEvent = Java.type("net.md_5.bungee.api.chat.HoverEvent");
-    var ClickEvent = Java.type("net.md_5.bungee.api.chat.ClickEvent");
-    var ComponentBuilder = Java.type("net.md_5.bungee.api.chat.ComponentBuilder");
+const TextComponent = Java.type("net.md_5.bungee.api.chat.TextComponent");
+const HoverEvent = Java.type("net.md_5.bungee.api.chat.HoverEvent");
+const ClickEvent = Java.type("net.md_5.bungee.api.chat.ClickEvent");
+const ComponentBuilder = Java.type("net.md_5.bungee.api.chat.ComponentBuilder");
+const ChatColor = Java.type("net.md_5.bungee.api.ChatColor");
 
-    this.message = new TextComponent(text);
-
-    this.setHoverText = hovText => {
-      this.message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hovText).create()));
-      return this;
+exports = class Text {
+    constructor(message){
+        this.message = new TextComponent(message);
     }
 
-    this.setCommand = command => {
-      this.message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command));
-      return this;
+    setHoverText(hovText){
+        this.message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hovText).create()));
+        return this;
     }
 
-    this.setUrl = url => {
-      this.message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
-      return this;
+    setCommand(command){
+        this.message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command));
+        return this;
     }
 
-    this.addExtra = text => {
-      this.message.addExtra(text);
-      return this;
+    setUrl(url){
+        this.message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+        return this;
     }
 
-    this.setSuggestCommand = command => {
-      this.message.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
-      return this;
+    addExtra(text){
+        this.message.addExtra(text);
+        return this;
     }
 
-    this.create = () => {
-      return this.message;
+    setSuggestCommand(command){
+        this.message.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
+        return this;
     }
 
-    return this;
-  }
+    setColor(color){
+        this.message.setColor(ChatColor.of(color));
+        return this;
+    }
+
+    setBold(bool){
+        this.message.setBold(bool);
+        return this;
+    }
+
+    setItalic(bool){
+        this.message.setItalic(bool);
+        return this;
+    }
+
+    setStrikethrough(bool){
+        this.message.setStrikethrough(book);
+        return this;
+    }
+
+    setUnderlined(bool){
+        this.message.setUnderlined(bool);
+        return this;
+    }
+
+    setObfuscated(bool){
+        this.message.setObfuscated(bool);
+        return this;
+    }
+
+    create(){
+        return this.message;
+    }
 }
